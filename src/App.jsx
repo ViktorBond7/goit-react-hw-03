1e343da47c174b804ae1e85c6667da3df2336d0b
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-03-31 09:23:36 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
